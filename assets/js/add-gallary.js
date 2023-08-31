c1d1d601b8bb1bfb1a412c49993c5d37440faee1
Raamishshah010 <raamishshah010@gmail.com>
2023-08-31 19:04:50 +0500

    working on admin panel

---
+++ assets/js/add-gallary.js
@@ -0,0 +1,74 @@
+let file = null;
+
+let image = document.getElementById('image');
+image.addEventListener('change', function (e) {
+
+    file = e.target.files[0];
+
+});
+
+
+
+function addGallery() {
+    
+
+    if ( file === null) return window.alert('Please Select Image');
+
+    $('#subBtn').html('Please Wait....');
+    $('#subBtn').addClass('disabled');
+
+    var storageRef = firebase.storage().ref('images/directorImage' + Date.now());
+
+    var uploadTask = storageRef.put(file);
+
+    uploadTask.on('state_changed',
+        (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+            $('#progressBar').html(`Uploaded ${progress}%`);
+            console.log('Upload is ' + progress + '% done');
+            console.log(snapshot.state)
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    console.log('Upload is paused');
+                    break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    console.log('Upload is running');
+                    break;
+            }
+        },
+        (error) => {
+            // Handle unsuccessful uploads
+            console.log(error)
+        },
+        () => {
+            uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                console.log('File available at', downloadURL);
+
+                let gallaryRef = db.collection("gallaryCollection").doc();
+
+                let data = {
+
+                    image: downloadURL,
+                    docID: gallaryRef.id
+
+                }
+
+                gallaryRef.set(data).then(() => {
+                    window.alert('Gallary Image Added Successfully')
+                    window.location.reload();
+                }).catch((error) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+
+                    console.log(errorMessage);
+                    window.alert(errorMessage)
+                });
+            })
+        })
+
+
+
+}
--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -35,3 +35,65 @@ db.collection('directorCollection').get().then((result) => {
 }).catch((err) => {
     window.alert(err.message);
 });
+
+
+
+let gallaryID = null;
+
+
+
+db.collection('gallaryCollection').get().then((result) => {
+
+    $('#gallaryCount').html(result.docs.length)
+    let i = 0;
+
+
+    result.forEach((director) => {
+        $(`#galleryList`).append(`
+        <div class="col-sm-12 col-md-6 col-lg-4 mt-5">
+                            <div class="gallaryImage">
+                                <img data-toggle="modal" data-target="#exampleModal" src="${director.data().image}" id="selectImage${i}" width="100%" alt="">
+                            </div>
+                        </div>
+        
+        `)
+
+
+
+        $(`#selectImage${i}`).on('click', function () {
+
+            $(`#gallaryImageModel`).attr('src', director.data().image);
+
+            
+            localStorage.setItem('gallaryID', result.data().docID)
+
+        })
+
+        i++;
+
+    });
+
+}).catch((err) => {
+    window.alert(err.message);
+});
+
+
+
+function deleteImage() {
+
+    gallaryID = localStorage.getItem('gallaryID')
+
+    let checkConfirm = confirm('Are you sure you want to delete');
+
+    if (checkConfirm) {
+        db.collection('gallaryCollection').doc(gallaryID).delete().then(() => {
+            window.alert('Deleted Successfully');
+            window.location.reload();
+        }).catch((err) => {
+            window.alert(err.message);
+        });
+    } else {
+        window.alert('Not Deleted')
+
+    }
+}
