011915eeb9fbbeb3439790e7acad791602e73e7d
Raamishshah010 <raamishshah010@gmail.com>
2023-09-05 22:08:50 +0500

    Deploying on Server

---
+++ assets/js/custom.js
@@ -0,0 +1,14 @@
+
+
+$('#toggleMenu').on('click', function(e) {
+
+    $('#mobileMenu').css('transform', "translateY(0)")
+})
+
+
+$('#closeBtn').on('click', function(e) {
+
+    $('#mobileMenu').css('transform', "translateY(-1000px)")
+})
+
+
